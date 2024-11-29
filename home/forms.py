ba7a8645692c7080adad7f01996126a4785fd935
ayhamrabea <ayhamrabea05@.com>
2024-11-29 16:23:25 +0300

    add bootstrap

--- home/forms.py
+++ home/forms.py
@@ -2,15 +2,17 @@ from django import forms
 from . import models
 
 
+atrs = {'class' : 'form-control'}
+
 class ProjectCreateForm(forms.ModelForm):
     
     class Meta:
         model = models.Project
         fields = ('category', 'title' ,'description')
         widgets = {
-            'category': forms.Select(),
-            'title' : forms.Textarea(),
-            'description': forms.Textarea()
+            'category': forms.Select(attrs=atrs),
+            'title' : forms.TextInput(attrs=atrs),
+            'description': forms.Textarea(attrs=atrs)
         }
 
 
@@ -21,8 +23,8 @@ class ProjectUpdateForm(forms.ModelForm):
         model = models.Project
         fields = ('category', 'title' ,'status')
         widgets = {
-            'category': forms.Select(),
-            'title' : forms.Textarea(),
-            'status': forms.Select()
+            'category': forms.Select(attrs=atrs),
+            'title' : forms.TextInput(attrs=atrs),
+            'status': forms.Select(attrs=atrs)
         }
 
--- project/settings.py
+++ project/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'home',
     'accounts',
+
 ]
 
 MIDDLEWARE = [
